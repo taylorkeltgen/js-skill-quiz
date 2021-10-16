c47a793abf505f11f2742a61ea279fb656e38a28
taylorkeltgen <taylorkeltgen@gmail.com>
2021-10-16 13:25:12 -0500

    finished styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
+var startButtonEl = document.getElementById('start-button');
+var startContainerEl = document.getElementById('start-container');
+var questionContainerEl = document.getElementById('question-container');
 
-var startGame = function(){
 
+
+var startGame = function() {
+    console.log('Game Start!');
+    startContainerEl.classList.add('hide');
+    questionContainerEl.classList.remove('hide');
 }
 
 var setNextQuestion = function() {
 
 }
 
+var selectAnswer = function() {
+
+}
+startButtonEl.addEventListener('click', startGame);
